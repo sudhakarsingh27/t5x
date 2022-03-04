4f3273bf9199d495b3c4bcffc4f397d41024ed70
Colin Gaffney <cpgaffney@google.com>
2022-03-04 14:42:02 -0800

    Add Checkpointer/CheckpointManager implementations that rely on t5x.checkpoints.Checkpointer to execute save and restore. This refactor should not change behavior.
    This represents an intermediate stage before creation of more generic APIs not specific to T5X.
    
    PiperOrigin-RevId: 432536203

--- setup.py
+++ setup.py
@@ -54,6 +54,7 @@ setuptools.setup(
         f'jax >= {_jax_version}',
         f'jaxlib >= {_jaxlib_version}',
         'numpy',
+        'orbax',
         'seqio-nightly',
         't5',
         'tensorflow',
